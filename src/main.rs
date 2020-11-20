8cc77c4bf844080039c709acf3db81fdd96d6a06
Duncan Freeman <dfreeman@lucidyne.com>
2020-11-19 17:23:27 -0800

    Line distance

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,36 @@ use klystron::{
 };
 use nalgebra::{Matrix4, Point2, Vector4};
 use rand::distributions::{Distribution, Uniform};
+use std::cmp::Ordering;
 
 type Point = Point2<f32>;
 type Line = (Point, Point);
 
 
-fn f32_cmp(a: f32, b: f32) -> std::cmp::Ordering {
-    a.partial_cmp(&b).unwrap_or(std::cmp::Ordering::Equal)
-}
-
-
 fn quickhull_init(points: &[Point]) -> Option<Line> {
-    let min = *points.iter().min_by(|a, b| f32_cmp(a.x, b.x))?;
-    let max = *points.iter().max_by(|a, b| f32_cmp(a.x, b.x))?;
+    fn pt_compare (a: &Point, b: &Point) -> Ordering {
+        f32_cmp(a.x, b.x).then(f32_cmp(a.y, b.y))
+    }
+    let min = *points.iter().min_by(|&a, &b| pt_compare(a, b))?;
+    let max = *points.iter().max_by(|&a, &b| pt_compare(a, b))?;
     Some((min, max))
 }
 
 fn quickhull(points: &[Point], line: &Line) -> Vec<Line> {
     if points.is_empty() { return vec![] }
     vec![]
+}
+
+fn f32_cmp(a: f32, b: f32) -> Ordering {
+    a.partial_cmp(&b).unwrap_or(Ordering::Equal)
+}
+
+// https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+fn line_dist((a, b): &Line, pt: Point) -> f32 {
+    let length = (a - b).magnitude();
+    let cross = b.x * a.y - b.y * a.x;
+    let numerator = ((b.y - a.y) * pt.x - (b.x - a.x) * pt.y + cross).abs();
+    numerator / length
 }
 
 fn line_right((a, b): Line, point: Point) -> bool {
@@ -122,16 +133,19 @@ impl App2D for MyApp {
             points.push(Point::new(x, y));
         }
 
+        let line = (Point::new(1., -1.), Point::new(-1., 1.));
         let vertices = points
             .iter()
-            .map(|p| point2d_to_vertex(*p, [1.; 3]))
+            //.map(|p| point2d_to_vertex(*p, [1.; 3]))
+            .map(|p| point2d_to_vertex(*p, [line_dist(&line, *p); 3]))
             .collect::<Vec<_>>();
         let indices = (0..vertices.len() as u16).collect::<Vec<_>>();
         let point_mesh = engine.add_mesh(&vertices, &indices)?;
 
         let init = quickhull_init(&points).expect("Empty set");
         let mut hull = quickhull(&points, &init);
-        hull.push(init);
+        //hull.push(init);
+        hull.push(line);
         let (vertices, indices) = lines_to_mesh(&hull, [0., 1., 0.]);
         let line_mesh = engine.add_mesh(&vertices, &indices)?;
 
