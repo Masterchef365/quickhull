27d095394f29c0b80e2ff5fa5b46e29f377d6481
Masterchef365 <duncan.freeman1@gmail.com>
2020-11-19 21:42:23 -0800

    Break out into a library

---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+use std::cmp::Ordering;
+
+pub type Point = nalgebra::Point2<f32>;
+pub type Line = (Point, Point);
+
+/// Calculate the convex hull from a set of points
+pub fn quickhull(points: &[Point]) -> Vec<Line> {
+    let mut lines = Vec::new();
+
+    let (init_line_a, init_line_b) = match quickhull_init(points) {
+        None => return lines,
+        Some(l) => l,
+    };
+
+    quickhull_recursive(points, (init_line_a, init_line_b), &mut lines);
+    quickhull_recursive(points, (init_line_b, init_line_a), &mut lines);
+
+    lines
+}
+
+/// Find initial line spanning the largest distance over the x axis, and y axis if there's any
+/// ambiguity.
+fn quickhull_init(points: &[Point]) -> Option<Line> {
+    fn pt_compare(a: &Point, b: &Point) -> Ordering {
+        f32_cmp(a.x, b.x).then(f32_cmp(a.y, b.y))
+    }
+    let min = *points.iter().min_by(|&a, &b| pt_compare(a, b))?;
+    let max = *points.iter().max_by(|&a, &b| pt_compare(a, b))?;
+    Some((min, max))
+}
+
+/// Recursively find the convex hull of this half of the given point set
+fn quickhull_recursive(points: &[Point], line: Line, out_lines: &mut Vec<Line>) {
+    let right = points
+        .iter()
+        .copied()
+        .filter(|&pt| line_right(line, pt))
+        .collect::<Vec<_>>();
+
+    let furthest = right
+        .iter()
+        .max_by(|&a, &b| f32_cmp(line_dist(line, *a), line_dist(line, *b)));
+
+    match furthest {
+        None => out_lines.push(line),
+        Some(furthest) => {
+            quickhull_recursive(&right, (*furthest, line.1), out_lines);
+            quickhull_recursive(&right, (line.0, *furthest), out_lines);
+        }
+    }
+}
+
+/// Compare two floats in a (semi) sane way
+fn f32_cmp(a: f32, b: f32) -> Ordering {
+    a.partial_cmp(&b).unwrap_or(Ordering::Equal)
+}
+
+/// Find the closest distance between a line and a given point
+fn line_dist((a, b): Line, pt: Point) -> f32 {
+    // https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+    let length = (a - b).magnitude();
+    let cross = b.x * a.y - b.y * a.x;
+    let numerator = ((b.y - a.y) * pt.x - (b.x - a.x) * pt.y + cross).abs();
+    numerator / length
+}
+
+/// Returns `true` when the given point is to the right of a given line
+fn line_right((a, b): Line, point: Point) -> bool {
+    let l = b - a;
+    let p = point - a;
+    l.x * p.y - l.y * p.x < 0.
+}
+
+#[cfg(test)]
+#[test]
+fn test_line_right() {
+    let line = (Point::new(1., -1.), Point::new(-1., 1.));
+    assert!(line_right(line, Point::new(20., 10.)));
+    assert!(!line_right(line, Point::new(-20., -10.)));
+    assert!(line_right(line, Point::new(20., -10.)));
+    assert!(!line_right(line, Point::new(-20., 10.)));
+}
--- src/main.rs
+++ src/main.rs
@@ -4,90 +4,9 @@ use klystron::{
     DrawType, Engine, FramePacket, Material, Mesh, Object, Vertex, WinitBackend, UNLIT_FRAG,
     UNLIT_VERT,
 };
-use nalgebra::{Matrix4, Point2, Vector4};
+use nalgebra::{Matrix4, Vector4};
+use quickhull::{quickhull, Line, Point};
 use rand::distributions::{Distribution, Uniform};
-use std::cmp::Ordering;
-
-type Point = Point2<f32>;
-type Line = (Point, Point);
-
-fn quickhull_init(points: &[Point]) -> Option<Line> {
-    fn pt_compare(a: &Point, b: &Point) -> Ordering {
-        f32_cmp(a.x, b.x).then(f32_cmp(a.y, b.y))
-    }
-    let min = *points.iter().min_by(|&a, &b| pt_compare(a, b))?;
-    let max = *points.iter().max_by(|&a, &b| pt_compare(a, b))?;
-    Some((min, max))
-}
-
-fn quickhull(points: &[Point], line: Line, out_lines: &mut Vec<Line>) {
-    let right = points
-        .iter()
-        .copied()
-        .filter(|&pt| line_right(line, pt))
-        .collect::<Vec<_>>();
-
-    let furthest = right
-        .iter()
-        .max_by(|&a, &b| f32_cmp(line_dist(line, *a), line_dist(line, *b)));
-
-    match furthest {
-        None => out_lines.push(line),
-        Some(furthest) => {
-            quickhull(&right, (*furthest, line.1), out_lines);
-            quickhull(&right, (line.0, *furthest), out_lines);
-        }
-    }
-}
-
-fn f32_cmp(a: f32, b: f32) -> Ordering {
-    a.partial_cmp(&b).unwrap_or(Ordering::Equal)
-}
-
-// https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
-fn line_dist((a, b): Line, pt: Point) -> f32 {
-    let length = (a - b).magnitude();
-    let cross = b.x * a.y - b.y * a.x;
-    let numerator = ((b.y - a.y) * pt.x - (b.x - a.x) * pt.y + cross).abs();
-    numerator / length
-}
-
-fn line_right((a, b): Line, point: Point) -> bool {
-    let l = b - a;
-    let p = point - a;
-    l.x * p.y - l.y * p.x < 0.
-}
-
-#[test]
-fn test_line_right() {
-    let line = (Point::new(1., -1.), Point::new(-1., 1.));
-    assert!(line_right(line, Point::new(20., 10.)));
-    assert!(!line_right(line, Point::new(-20., -10.)));
-    assert!(line_right(line, Point::new(20., -10.)));
-    assert!(!line_right(line, Point::new(-20., 10.)));
-}
-
-#[test]
-fn test_triangle_member() {
-    let a = Point::new(0., 1.);
-    let b = Point::new(1., -1.);
-    let c = Point::new(-1., -1.);
-    let test_all_order = |pt| {
-        triangle_member(a, b, c, pt)
-            && triangle_member(b, c, a, pt)
-            && triangle_member(c, a, b, pt)
-            && triangle_member(a, c, b, pt)
-            && triangle_member(b, a, c, pt)
-            && triangle_member(c, b, a, pt)
-    };
-    assert!(test_all_order(Point2::origin()));
-    assert!(test_all_order(Point2::new(0.1, 0.1)));
-    assert!(!test_all_order(Point2::new(5., 5.)));
-    assert!(!test_all_order(Point2::new(-5., 5.)));
-    assert!(!test_all_order(Point2::new(-5., -5.)));
-}
-
-// ######################################################################
 
 struct MyApp {
     point_material: Material,
@@ -96,27 +15,8 @@ struct MyApp {
     line_mesh: Mesh,
 }
 
-fn point2d_to_vertex(point: Point, color: [f32; 3]) -> Vertex {
-    Vertex {
-        pos: [point.x, point.y, 0.],
-        color,
-    }
-}
-
-fn lines_to_mesh(lines: &[Line], color: [f32; 3]) -> (Vec<Vertex>, Vec<u16>) {
-    let mut vertices = Vec::with_capacity(lines.len() * 2);
-    let mut indices = Vec::with_capacity(lines.len() * 2);
-    for (a, b) in lines {
-        vertices.push(point2d_to_vertex(*a, color));
-        indices.push(indices.len() as _);
-        vertices.push(point2d_to_vertex(*b, color));
-        indices.push(indices.len() as _);
-    }
-    (vertices, indices)
-}
-
 impl App2D for MyApp {
-    const TITLE: &'static str = "2D example app";
+    const TITLE: &'static str = "Quickhull";
     type Args = ();
 
     fn new(engine: &mut WinitBackend, _args: Self::Args) -> Result<Self> {
@@ -124,36 +24,31 @@ impl App2D for MyApp {
         let point_material = engine.add_material(UNLIT_VERT, UNLIT_FRAG, DrawType::Points)?;
 
         let mut rng = rand::thread_rng();
-        let count = 10_000_000;
+        let count = 1000;
         let dist_unif = Uniform::new(0., 1.);
         let angle_unif = Uniform::new(0., std::f32::consts::TAU);
         let mut points = Vec::new();
         for _ in 0..count {
             let dist = dist_unif.sample(&mut rng);
             let angle = angle_unif.sample(&mut rng);
-            let x = angle.cos() * dist;
+            let x = angle.cos() * dist * (angle * 2.).sin();
             let y = angle.sin() * dist;
             points.push(Point::new(x, y));
         }
 
-        let vertices = points[..40_000]
+        let vertices = points[..points.len().min(40_000)]
             .iter()
             .map(|p| point2d_to_vertex(*p, [1.; 3]))
             .collect::<Vec<_>>();
         let indices = (0..vertices.len() as u16).collect::<Vec<_>>();
         let point_mesh = engine.add_mesh(&vertices, &indices)?;
 
-        let init = quickhull_init(&points).expect("Empty set");
-
         use std::time::Instant;
         let begin = Instant::now();
-        let mut hull = Vec::new();
-        quickhull(&points, init, &mut hull);
-        quickhull(&points, (init.1, init.0), &mut hull);
+        let hull = quickhull(&points);
         let end = Instant::now();
         println!("Time: {:?}", end - begin);
 
-        //hull.push(init);
         let (vertices, indices) = lines_to_mesh(&hull, [0., 1., 0.]);
         let line_mesh = engine.add_mesh(&vertices, &indices)?;
 
@@ -163,10 +58,6 @@ impl App2D for MyApp {
             line_mesh,
             line_material,
         })
-    }
-
-    fn event(&mut self, _event: &WindowEvent, _engine: &mut WinitBackend) -> Result<()> {
-        Ok(())
     }
 
     fn frame(&self) -> FramePacket {
@@ -191,8 +82,31 @@ impl App2D for MyApp {
             objects: vec![points, lines],
         }
     }
+
+    fn event(&mut self, _event: &WindowEvent, _engine: &mut WinitBackend) -> Result<()> {
+        Ok(())
+    }
 }
 
 fn main() -> Result<()> {
     launch::<MyApp>(())
+}
+
+fn point2d_to_vertex(point: Point, color: [f32; 3]) -> Vertex {
+    Vertex {
+        pos: [point.x, point.y, 0.],
+        color,
+    }
+}
+
+fn lines_to_mesh(lines: &[Line], color: [f32; 3]) -> (Vec<Vertex>, Vec<u16>) {
+    let mut vertices = Vec::with_capacity(lines.len() * 2);
+    let mut indices = Vec::with_capacity(lines.len() * 2);
+    for (a, b) in lines {
+        vertices.push(point2d_to_vertex(*a, color));
+        indices.push(indices.len() as _);
+        vertices.push(point2d_to_vertex(*b, color));
+        indices.push(indices.len() as _);
+    }
+    (vertices, indices)
 }
